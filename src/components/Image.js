30e75a30693e49020d391919782f6f250ab74b2c
William Cragg <wstc@riseup.net>
2020-11-09 18:55:20 +0000

    gallery sections dynamically generated

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,41 +1,17 @@
 import React from 'react';
-import axios from 'axios';
 
 
-class ImageThumb extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            image: '',
-            error: undefined,
-        }
-    }
-    componentDidMount() {
-        console.log(this.props.url)
 
-
-        axios.get(this.props.url)
-            // const data = await response;
-            .then(res => {
-                this.setState({
-                    image: res.data
-                })
-            })
-            .catch();
-
-    }
-
-    render() {
-
-        return (
-            <div className="fl w-50 w-third-m w-25-ns">
+const ImageThumb = ({ url }) => {
+	console.log(url)
+  return(
+ <div className="fl w-50 w-third-m w-25-ns">
     <div className="aspect-ratio aspect-ratio--1x1">
-      <img alt="" style={{'backgroundImage':'url('+this.props.url+')'}}
+      <img alt="" style={{'backgroundImage':'url('+url+')'}}
         className="db bg-center cover aspect-ratio--object" />
     </div>
   </div>
-        )
-    }
+  )
 }
 
 export default ImageThumb;
--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,57 +1,49 @@
 import React from 'react';
-import axios from 'axios';
 import ImageThumb from '../components/Image';
-import kitchen1 from '../images/kitchen-photo-1.jpg'
-import kitchen2 from '../images/kitchen-photo-2.jpg'
-import kitchen3 from '../images/kitchen-photo-3.jpg'
-import kitchen4 from '../images/kitchen-photo-4.jpg'
 
 class Gallery extends React.Component {
     constructor() {
         super();
         this.state = {
-            photosData: []
+            photosData: ['1325', '22', '3666', '436', '57474', '622', '72', '86'],
+            category: ['Shelving', 'Kitchens', 'Cabinetry', 'Woodturning', 'Furniture', 'Carving', 'Outdoor Builds', 'Workshop', 'Framing']
         }
     }
-    componentDidMount() {
-        axios.get('https://picsum.photos/v2/list')
-            .then(res => {
-                this.setState({
-                    photosData: res.data
-                })
-            })
-    }
+
     render() {
         var images = this.state.photosData
+        var category = this.state.category
 
         return (
             <div>
+
   <h1>Gallery</h1>
   {
-              images.map((image, i) => {
-              return(
-                <ImageThumb key={i} url={image.download_url}></ImageThumb>
-                   )
-                })
+  category.map((category, i) => {
+    return (
+      <div>
+      <h2 key={i}>{category}</h2>
+      <main className="cf w-100">
+ {       
+        images.map((image, i) => {
+              return( 
+                
+                <ImageThumb key={i} url={'https://picsum.photos/seed/'+ image +'/150'}/>
+             
+                )})
+
   }
-  <h2>Shelving</h2>
-  <h2>Kitchens</h2>
-  <main className="cf w-100">
-    <ImageThumb url={kitchen1} ></ImageThumb>
-    <ImageThumb url={kitchen2} ></ImageThumb>
-    <ImageThumb url={kitchen3} ></ImageThumb>
-    <ImageThumb url={kitchen4} ></ImageThumb>
-  </main>
-  <h2>Cabinetry</h2>
-  <h2>Woodturning</h2>
-  <h2>Furniture</h2>
-  <h2>Carving</h2>
-  <h2>Outdoor Builds</h2>
-  <h2>Workshop</h2>
-  <h2>Framing</h2>
+   
+      </main>
+      </div>
+      )
+  })
+}
+
 </div>
 
         );
     }
 }
+
 export default Gallery;
