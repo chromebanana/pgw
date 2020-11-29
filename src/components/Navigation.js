2854b3d526da468f997ef0ed6b347ac38c04d7bb
William Cragg <wstc@riseup.net>
2020-11-29 18:02:32 +0000

    sticky nav wip

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from "react";
+
 import pgwname from '../images/pgw-name.png';
 
 
+
 export const Navigation = () => {
+
+  useEffect(() => {
+    const header = document.getElementById("header");
+    const sticky = header.offsetTop;
+    const scrollCallBack = window.addEventListener("scroll", () => {
+      if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+      } else {
+        header.classList.remove("sticky");
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll", scrollCallBack);
+    };
+  }, []);
+
+
   return (
   	<div className="pv4-ns pv3 bb b--black-20 ">
 
 	<img class="w-100" alt="pgw"src={pgwname}/>
-	<div>
+	<div id="header">
 		<nav  className="mw7 mh0 mv2 db tr">
 		<Link className='dim black underline f2-ns f3 dib ml3 pointer mv2' to="/">Home</Link>
 		<Link className='dim black underline f2-ns f3 dib ml3 pointer mv2' to="/gallery">Gallery</Link>
