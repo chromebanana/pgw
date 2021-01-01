ca2889cf098705c4b6994287092c090b2535a57a
William Cragg <wstc@riseup.net>
2021-01-01 23:23:02 +0000

    padding

--- src/components/MemberCard.js
+++ src/components/MemberCard.js
@@ -8,12 +8,14 @@ const MemberCard = ({ name, trades, tool, img }) => {
  <div className="mw5 center br3 pa3 pa4-ns mv2 ba b--black-10" style={{'backgroundImage':`url(${wood})`}}>
  	<div className="tc">
  	<img src={img} className="br-100 h4 w4 dib" title={"Photo of "+name} alt={"photo of "+name}/>
-	<h1 className="f4">{name}</h1>
-	<hr className="mw4 bb bw1 b--black-30 mb3"/>
- 	</div>
- 	<div className="lh-copy ma0 pa2 center f5 black-70 bg-white-70 br3">
+	<div className=" bg-white-70 br3 ">
+	<h1 className="f4 pt2 mb2">{name}</h1>
+	<hr className="mw4 bb bw1 b--black-30 mv0"/>
+ 	<div className="lh-copy ma0 pa2 center f5 black-7">
    <p className="ma0 pv1"><span className="b">Trades:</span> {trades}</p>
    <p className="ma0 pv1"><span className="b">Favourite Tool:</span> {tool}</p>
+ 	</div>
+ </div>
   </div>
   </div>
   )
